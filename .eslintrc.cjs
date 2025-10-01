cb32dce26125a846eeb649b43661e1e234ff628d
AirKyzzZ <airkyzzz.jiji@gmail.com>
2025-10-01 15:13:20 +0200

    chore: update ESLint configuration to enhance TypeScript support and disable specific rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,10 +7,22 @@ module.exports = {
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+    sourceType: 'module',
+  },
   plugins: ['@typescript-eslint'],
   ignorePatterns: ['node_modules/', 'dist/', 'build/', 'coverage/', '.expo/', '.expo-shared/'],
   rules: {
     'no-console': ['warn', { allow: ['warn', 'error'] }],
+    '@typescript-eslint/no-throw-literal': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-require-imports': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    'import/order': 'off',
+    'prettier/prettier': 'off',
   },
 };
 
