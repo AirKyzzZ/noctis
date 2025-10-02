510d05c957a4c83a28f98d95e99c19658ac822f4
AirKyzzZ <airkyzzz.jiji@gmail.com>
2025-10-02 14:16:39 +0200

    refactor: remove old Layout component and update import path in App component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo, useState } from 'react';
 import { View } from 'react-native';
-import BottomNavbar, { type BottomTabItem } from '../../components/BottomNavbar';
+import BottomNavbar, { type BottomTabItem } from '../components/BottomNavbar';
 
 type LayoutProps = {
   children: React.ReactNode;
